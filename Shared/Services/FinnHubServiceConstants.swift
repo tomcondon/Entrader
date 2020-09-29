2d94643071b81ea776b3670cbf02e4aa0a575f59
Thomas Condon <tomcondon@mac.com>
2020-09-28 19:19:35 -0600

    First Commit

---
+++ Shared/Services/FinnHubServiceConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ServiceConstants.swift
+//  EnTrader
+//
+//  Created by Thomas Condon on 9/18/20.
+//
+
+import Foundation
+
+let apiKey = "btij9jv48v6ula7eji2g"
+
+let webhookSecret = "btij9jv48v6ula7eji30"
+
+let webhookSecretKeyPair = ["X-Finnhub-Secret": "btij9jv48v6ula7eji30"]
